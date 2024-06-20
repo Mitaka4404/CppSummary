3760f481a1461bdffd919fd6b8c6590a70ce0b52
Junhwan Kim <mitaka@gmail.com>
2024-06-20 16:17:11 +0900

    Hello, World

---
+++ Ex01_HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // iostream이라는 헤더를 포함(include)
+
+using namespace std; // 네임스페이스 설명 std::cout
+
+int main() //entry point
+{
+	// comment
+
+	cout << "Hello, World!" << endl;
+	// printf("Hello World!!! by printf");
+
+	char user_input[100];
+	cin >> user_input;
+	cout << user_input;
+
+	return 0;
+}
